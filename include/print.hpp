df1cbb489f2fb499c609469943e69c41d3ffb634
NorthDakota11 <151087787+NorthDakota11@users.noreply.github.com>
2026-05-28 18:51:50 +0300

    Add print header

---
+++ include/print.hpp
@@ -0,0 +1,8 @@
+#pragma once
+
+#include <ostream>
+#include <fstream>
+#include <string>
+
+void print(const std::string& text, std::ofstream& out);
+void print(const std::string& text, std::ostream& out);
