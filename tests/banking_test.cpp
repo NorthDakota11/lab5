b9dee080419c8cbd642ca8d2b6a3291790a3c070
NorthDakota11 <151087787+NorthDakota11@users.noreply.github.com>
2026-06-08 23:15:54 +0300

    Перевести тесты banking на GTest

--- tests/banking_test.cpp
+++ tests/banking_test.cpp
@@ -1,32 +1,24 @@
 #include "Account.h"
 #include "Transaction.h"
 
-#include <cassert>
+#include <gtest/gtest.h>
 #include <stdexcept>
 
-int main() {
+TEST(Banking, AccountAndTransaction)
+{
   Account account{1, 100};
-  assert(account.id() == 1);
-  assert(account.GetBalance() == 100);
+  EXPECT_EQ(account.id(), 1);
+  EXPECT_EQ(account.GetBalance(), 100);
 
-  bool unlocked_error = false;
-  try {
-    account.ChangeBalance(10);
-  } catch (const std::runtime_error&) {
-    unlocked_error = true;
-  }
-  assert(unlocked_error);
+  EXPECT_THROW(account.ChangeBalance(10), std::runtime_error);
 
   account.Lock();
   account.ChangeBalance(50);
   account.Unlock();
-  assert(account.GetBalance() == 150);
+  EXPECT_EQ(account.GetBalance(), 150);
 
   Account from{2, 500};
   Account to{3, 100};
   Transaction transaction;
-  bool result = transaction.Make(from, to, 100);
-  assert(result);
-
-  return 0;
+  EXPECT_TRUE(transaction.Make(from, to, 100));
 }
